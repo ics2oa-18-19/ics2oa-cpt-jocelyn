741b33a53eaae21aef6299317890c8626cab4e05
Jocelyn <47438128+JJace421@users.noreply.github.com>
2019-05-28 10:37:16 -0400

    Update cpt_edits.py

--- dev/cpt_edits.py
+++ dev/cpt_edits.py
@@ -1,14 +1,16 @@
 import arcade
 
 
-WIDTH = 640
-HEIGHT = 480
+WIDTH = 500
+HEIGHT = 650
 
-player_x = 320
-player_y = 100
+player_x = WIDTH / 2
+player_y = 50
 
 left_key = False
 right_key = False
+up_key = False
+down_key = False
 
 def setup():
     arcade.open_window(WIDTH, HEIGHT, "My Arcade Game")
@@ -26,17 +28,26 @@ def setup():
 
 
 def update(delta_time):
-    global left_key, right_key, player_x
+    global left_key, right_key, player_x, player_y
 
+    speed = 8
     if left_key == True:
-        player_x -= 7
+        player_x -= speed
     elif right_key == True:
-        player_x += 7
+        player_x += speed
+    elif up_key == True:
+        player_y += speed
+    elif down_key == True:
+        player_y -= speed
 
-    if player_x < 0:
-        player_x = 0
-    elif player_x > WIDTH:
-        player_x = WIDTH
+    if player_x < 25:
+        player_x = 25
+    elif player_x > WIDTH - 25:
+        player_x = WIDTH - 25
+    elif player_y < 25:
+        player_y = 25
+    elif player_y > 100:
+        player_y = 100
 
 
 def on_draw():
@@ -44,30 +55,41 @@ def on_draw():
     # Draw in here...
     global player_x, player_y
 
-    arcade.draw_circle_filled(player_x, player_y, 30, arcade.color.BLUE)
+    arcade.draw_rectangle_filled(player_x, player_y, 45,45, arcade.color.BLUE)
+    arcade.draw_line(400, HEIGHT,WIDTH,120,arcade.color.NAVY_BLUE)
+    arcade.draw_line(100, HEIGHT,0,120,arcade.color.NAVY_BLUE)
 
 
 def on_key_press(key, modifiers):
-    global left_key, right_key
+    global left_key, right_key, up_key, down_key
 
     if key == arcade.key.LEFT:
         left_key = True
     elif key == arcade.key.RIGHT:
         right_key = True
+    elif key == arcade.key.UP:
+        up_key = True
+    elif key == arcade.key.DOWN:
+        down_key = True
 
 
 
 def on_key_release(key, modifiers):
-    global left_key, right_key
+    global left_key, right_key, up_key, down_key
 
     if key == arcade.key.LEFT:
         left_key = False
     elif key == arcade.key.RIGHT:
         right_key = False
+    elif key == arcade.key.UP:
+        up_key = False
+    elif key == arcade.key.DOWN:
+        down_key = False
 
 
 def on_mouse_press(x, y, button, modifiers):
     pass
+
 
 
 if __name__ == '__main__':
